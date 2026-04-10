283aedf2a92a4f6d5a6f0fee15698ccef364e798
Esteban Ochoa <esteban8adela@gmail.com>
2026-04-10 08:57:12 -0600

    Aprendiendo matchers

--- src/mockData.ts
+++ src/mockData.ts
@@ -6,3 +6,9 @@ export const objData: IObjData = {
     name: 'Jose',
     age: 21
 }
+
+export const fetchData = {
+    name: 'Jose',
+    ID: 2,
+    email: 'jose@example.com'
+}
--- src/usingMatchers/usingMatchers.test.ts
+++ src/usingMatchers/usingMatchers.test.ts
@@ -1,17 +1,18 @@
 import { expect, test, describe } from 'vitest';
-import { objData } from '../mockData';
+import { objData, fetchData } from '../mockData';
 
-describe( 'learning common matchers', () => {
+describe('learning common matchers', () => {
     test('Comparing objects', () => {
         // toBe for primitives
         // toEqual for objects and arrays, duples, sets, maps, etc
+
         const temp = { name: 'Jose', age: 21 };
         expect(objData).toEqual(temp)
-        //expect(objData).toBe({temp})
+        //expect(objData).toBe(temp)
     });
 
     test('Number Comparisons', () => {
-        const value = 2+2;
+        const value = 2 + 2;
         expect(value).toBeGreaterThan(3);
         expect(value).toBeGreaterThanOrEqual(3);
         expect(value).toBeLessThanOrEqual(4);
@@ -22,7 +23,50 @@ describe( 'learning common matchers', () => {
     test('adding floating point numbers', () => {
         const value = 0.1 + 0.2;
         //expect(value).toBe(0.3); // This won't work because of rounding error
-        expect(value).toBeCloseTo(0.3); // This works.
+        expect(value).toBeCloseTo(0.3, 3); // This works.
+    })
+
+    test('There is no I in team', () => {
+        //expect('team').toMatch(/I/); // This won't work because there is no "I" in "team"
+        expect('team').not.toMatch(/I/);
+    })
+
+    test('version string matches format', () => {
+        expect('vitest@1.0.0').toMatch(/vitest@\d+\.\d+\.\d+/);
+    })
+
+    test('null checks', () => {
+        const n = null;
+        const und = undefined;
+
+        expect(n).toBeNull(); //toBe(null)
+        expect(und).toBeUndefined();
+        expect(n).toBeFalsy();
+        expect(und).toBeFalsy();
+        expect(n).toBeNullable();
+        expect(und).toBeNullable();
+
+    })
+
+    test('Check endpoint', () => {
+        expect(fetchData.name).toBe('Jose'); // Funciona pero es incorrecto
+        expect(fetchData).toMatchObject({ // Mejor forma
+            name: 'Jose'
+        });
+        expect(fetchData).toHaveProperty('name'); // Solo verifica que la propiedad exista, no su valor
+        expect(fetchData).toHaveProperty('name', 'Jose'); // Verifica que la propiedad exista y tenga el valor esperado
+    })
+
+    test('The shopping list has milk on it', () => {
+        const shoppingList = [
+            'diapers',
+            'kleenex',
+            'trash bags',
+            'paper towels',
+            'milk'
+        ];
+
+        expect(shoppingList).toContain('milk');
     })
 
 });
